8aa5e2fab796fcccdd4ef7e8ecf11d5ef8d98eaa
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-03-24 23:14:44 +0300

    Introduction about react-typeScript

---
+++ src/ReactTypeScript_Intro.tsx
@@ -0,0 +1,27 @@
+/* 
+    *** Why TypeScript with ReactJS ***
+        => With static type checking, you get to learn potential bugs as you are typing the code, 
+        then heading to the browser and figuring out at runtime.
+
+        => Provides a way to describe the shape of an object hence providing better documentation
+         and autocomplete.
+
+        => Makes maintenance and refactoring of large code bases much easier.
+
+    *** What is the problem of using of TypeScript ***
+        => I have to write a lot more code than i normally do and the compiler keeps complaining. 
+        I simply do not know what to do about those error.
+        => It does not seem to be productive for me.
+
+*/
+const ReactTypeScript_Intro = () => {
+    return (
+        <>
+            <h2>
+                Introduction About TypeScript why we use TypeScript With React
+            </h2>
+        </>
+    );
+};
+
+export default ReactTypeScript_Intro;
